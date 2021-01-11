dcaba50164703f02a8c449ffcbadd85dddd5f1a1
chakccheng <77304163+chakccheng@users.noreply.github.com>
2021-01-11 17:39:45 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Chak Cheng!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
